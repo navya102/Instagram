b6049846334ca8591369684a580e36ca5ed58bd8
navya102 <91990987+navya102@users.noreply.github.com>
2022-04-12 01:51:01 +0530

    Update CreatePost.js

--- client/src/components/screens/CreatePost.js
+++ client/src/components/screens/CreatePost.js
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from 'react'
-
+import M from 'materialize-css'
 import {useHistory} from 'react-router-dom'
 const CretePost = ()=>{
     const history = useHistory()
