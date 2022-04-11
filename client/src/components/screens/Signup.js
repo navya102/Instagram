9b0ab0a14949621160521af9794f0cb2a3f0ec14
navya102 <91990987+navya102@users.noreply.github.com>
2022-04-12 01:52:32 +0530

    Update Signup.js

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import {Link,useHistory} from 'react-router-dom'
-
+import M from 'materialize-css'
 const Signup  = ()=>{
     const history = useHistory()
     const [name,setName] = useState("")
